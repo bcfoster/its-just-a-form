e69c01162ad3ab4ba32dc9f8c57507309d1a1fee
Brent Foster <hello@brentfoster.dev>
2025-07-15 23:33:13 -0700

    install tailwind
    npm install tailwindcss @tailwindcss/postcss postcss --force

--- src/app/app.ts
+++ src/app/app.ts
@@ -21,15 +21,11 @@ import { MatTabsModule } from '@angular/material/tabs';
     MatTabsModule,
   ],
   template: `
-    <mat-drawer-container class="example-container">
+    <mat-drawer-container class="h-full">
       <mat-drawer mode="side" opened>
-        <mat-list
-          cdkDropList
-          class="example-list"
-          (cdkDropListDropped)="drop($event)"
-        >
+        <mat-list cdkDropList (cdkDropListDropped)="drop($event)">
           @for (step of steps; track step) {
-            <app-form-step-list-item class="example-box" [step]="step" />
+            <app-form-step-list-item class="cursor-move" [step]="step" />
           }
         </mat-list>
       </mat-drawer>
@@ -39,55 +35,6 @@ import { MatTabsModule } from '@angular/material/tabs';
         </mat-tab-group>
       </mat-drawer-content>
     </mat-drawer-container>
-  `,
-  styles: `
-    .example-list {
-      width: 500px;
-      max-width: 100%;
-      border: solid 1px #ccc;
-    }
-
-    .example-box {
-      border-bottom: solid 1px #ccc;
-      color: rgba(0, 0, 0, 0.87);
-      display: flex;
-      flex-direction: row;
-      align-items: center;
-      justify-content: space-between;
-      box-sizing: border-box;
-      cursor: move;
-    }
-
-    .cdk-drag-preview {
-      border: none;
-      box-sizing: border-box;
-      border-radius: 4px;
-      box-shadow:
-        0 5px 5px -3px rgba(0, 0, 0, 0.2),
-        0 8px 10px 1px rgba(0, 0, 0, 0.14),
-        0 3px 14px 2px rgba(0, 0, 0, 0.12);
-    }
-
-    .cdk-drag-placeholder {
-      opacity: 0;
-    }
-
-    .cdk-drag-animating {
-      transition: transform 250ms cubic-bezier(0, 0, 0.2, 1);
-    }
-
-    .example-box:last-child {
-      border: none;
-    }
-
-    .example-list.cdk-drop-list-dragging
-      .example-box:not(.cdk-drag-placeholder) {
-      transition: transform 250ms cubic-bezier(0, 0, 0.2, 1);
-    }
-
-    .example-container {
-      min-height: 100%;
-    }
   `,
 })
 export class App {
