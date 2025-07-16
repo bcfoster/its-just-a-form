2650d361596766eb67bdc080264648dfaf7aa00f
Brent Foster <hello@brentfoster.dev>
2025-07-15 23:43:15 -0700

    install ngrx
    ng add @ngrx/store@20.0.0-rc.0
    ng add @ngrx/effects@20.0.0-rc.0
    ng add @ngrx/store-devtools@20.0.0-rc.0
    ng add @ngrx/eslint-plugin@20.0.0-rc.0

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,15 +2,22 @@ import {
   ApplicationConfig,
   provideBrowserGlobalErrorListeners,
   provideZonelessChangeDetection,
+  isDevMode,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideRouter(routes),
+    provideEffects(),
+    provideStore(),
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() }),
   ],
 };
